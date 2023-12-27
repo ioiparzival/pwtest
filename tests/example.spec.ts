2458f03b5c5ece00e2e02fad71aab43dc7ab457f
ioiparzival <opshytoshnikita@gmail.com>
2023-12-27 12:29:46 +0200

    refactore: add wait 5sec

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,7 +5,9 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+  await page.waitForTimeout(5000);
 });
+
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
